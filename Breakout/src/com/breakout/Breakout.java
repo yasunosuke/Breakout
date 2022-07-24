c17e2717b37abba928f3bc20038c632329265726
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-07-24 22:56:51 +0900

    feat: new method
    Update paddle position x to follow the ball

--- Breakout/src/com/breakout/Breakout.java
+++ Breakout/src/com/breakout/Breakout.java
@@ -7,7 +7,7 @@ public class Breakout {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Velocity v = new Velocity(1,1);
-		Board b = new Board();
+		Board board = new Board();
 		Scanner sc = new Scanner(System.in);
 		
 		long time1 = System.currentTimeMillis();
@@ -15,23 +15,29 @@ public class Breakout {
 		while(true) {
 			long time2 = System.currentTimeMillis();
 			if(time2 > 1000/10 + time1) {
-				DrawnItem ball = b.getBall();
+				DrawnItem ball = board.getBall();
+				DrawnItem paddle = board.getPaddle();
 //				ball.setPositionX(ball.getPositionX() + v.getVelocityX());
 //				ball.setPositionY(ball.getPositionY() + v.getVelocityY());
-				updateBallPosition(ball, v);
+				updateBallPosition(ball, paddle, v);
+				updatePaddlePosition(ball, paddle);
 				time1 = time2;
-				b.draw();
+				board.draw();
 			}
 		}
 	}
 	
-	private static void updateBallPosition(DrawnItem ball, Velocity v) {
+	private static void updateBallPosition(DrawnItem ball, DrawnItem paddle, Velocity v) {
 		ball.setPositionX(ball.getPositionX() + v.getVelocityX());
 		ball.setPositionY(ball.getPositionY() + v.getVelocityY());
 		
 		int ballPositionX = ball.getPositionX();
 		int ballPositionY = ball.getPositionY();
 		
+		int paddlePositionX = paddle.getPositionX();
+		int paddlePositionY = paddle.getPositionY();
+		
+//		壁とのあたり判定
 		if(ballPositionX >= Board.BOARD_WIDTH - 1) {
 			v.setVelocityX(-1);
 		}
@@ -43,6 +49,14 @@ public class Breakout {
 		}
 		if(ballPositionY <= 0) {
 			v.setVelocityY(1);
+		}
+		
+//		パドルとの当たり判定
+		if((ballPositionY <= paddlePositionY) 
+				&& (ballPositionY >= paddlePositionY - 1) 
+				&& (ballPositionX >= paddlePositionX) 
+				&& (ballPositionX < paddlePositionX + paddle.getWIDTH())) {
+			v.setVelocityY(-1);
 		}
 		
 //	　　　　跳ね返る時に２フレーム描画する
@@ -59,6 +73,22 @@ public class Breakout {
 //			ball.setPositionX(nextPositionX);
 //			ball.setPositionY(nextPositionY);
 //		}
+		
+	}
+	
+//	ボールを追跡するためにパドルの位置を更新するメソッド
+	private static void updatePaddlePosition(DrawnItem ball, DrawnItem paddle) {
+//		パドルの壁との当たり判定
+//		ボールとパドルの真ん中を合わせる
+		int nextPaddlePositionX = ball.getPositionX() - 1;
+		if(nextPaddlePositionX + paddle.getWIDTH() - 1 >= Board.BOARD_WIDTH) {
+			paddle.setPositionX(nextPaddlePositionX - 1);
+		} else if(nextPaddlePositionX < 0) {
+			paddle.setPositionX(0);
+		} else {
+			paddle.setPositionX(nextPaddlePositionX);
+		}
+		
 		
 	}
 	
