1fef0e76c7b1d410f3a2843c85447a643551cc24
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-08-04 00:25:00 +0900

    feat: Put while loop and added a method that ask the player if he continue the game
    to continue the game even after you finished the game

--- Breakout/src/com/breakout/Breakout.java
+++ Breakout/src/com/breakout/Breakout.java
@@ -5,91 +5,71 @@ import java.util.Scanner;
 public class Breakout {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Velocity v = new Velocity(1,1);
-		Board board = new Board();
+		
 		Scanner sc = new Scanner(System.in);
 		
-		long time1 = System.currentTimeMillis();
-		
 		while(true) {
-			long time2 = System.currentTimeMillis();
-			if(time2 > 1000/10 + time1) {
-				DrawnItem ball = board.getBall();
-				DrawnItem paddle = board.getPaddle();
-//				ball.setPositionX(ball.getPositionX() + v.getVelocityX());
-//				ball.setPositionY(ball.getPositionY() + v.getVelocityY());
-				updateBallPosition(ball, paddle, v);
-				updatePaddlePosition(ball, paddle);
-				time1 = time2;
-				board.draw();
+			
+			Velocity v = new Velocity(1,1);
+			Board board = new Board();
+			
+			long time1 = System.currentTimeMillis();
+			
+			while(true) {
+				
+				long time2 = System.currentTimeMillis();
+				
+				if(time2 > 1000/10 + time1) {
+
+					board.updateBallPosition(v);
+					board.updatePaddlePosition();
+					
+					time1 = time2;
+					
+					board.draw();
+					
+					if(board.isFailed()) {
+						System.out.println("Game Over!");
+						System.out.println("You Broke " + board.countBlocks() + " Blocks!");
+						break;
+					}
+					
+					if(board.isCompleted()) {
+						System.out.println("Completed!");
+						break;
+					}
+				}
+			}
+			
+			if (isOneMoreGame(sc)) {
+				continue;
+			} else {
+				System.out.println("See you again!");
+				break;
 			}
 		}
 	}
-	
-	private static void updateBallPosition(DrawnItem ball, DrawnItem paddle, Velocity v) {
-		ball.setPositionX(ball.getPositionX() + v.getVelocityX());
-		ball.setPositionY(ball.getPositionY() + v.getVelocityY());
+			
+	private static boolean isOneMoreGame(Scanner sc) {
 		
-		int ballPositionX = ball.getPositionX();
-		int ballPositionY = ball.getPositionY();
-		
-		int paddlePositionX = paddle.getPositionX();
-		int paddlePositionY = paddle.getPositionY();
-		
-//		壁とのあたり判定
-		if(ballPositionX >= Board.BOARD_WIDTH - 1) {
-			v.setVelocityX(-1);
+		while (true) {
+			System.out.println("Wanna Continue? : Yes---1 No---0");
+			try {
+				int input = Integer.parseInt(sc.nextLine());
+				if (0 <= input && input <= 1) {
+					if (input == 1) {
+						return true;
+					} else {
+						return false;
+					}
+				} else {
+					System.out.println("Incorrect Input! Please Enter again.");
+				}
+				
+			} catch (NumberFormatException e) {
+				System.out.println("Incorrect Input! Please Enter again.");
+			}
 		}
-		if(ballPositionX <= 0) {
-			v.setVelocityX(1);
-		}
-		if(ballPositionY >= Board.BOARD_HEIGHT - 1) {
-			v.setVelocityY(-1);
-		}
-		if(ballPositionY <= 0) {
-			v.setVelocityY(1);
-		}
-		
-//		パドルとの当たり判定
-		if((ballPositionY <= paddlePositionY) 
-				&& (ballPositionY >= paddlePositionY - 1) 
-				&& (ballPositionX >= paddlePositionX) 
-				&& (ballPositionX < paddlePositionX + paddle.getWIDTH())) {
-			v.setVelocityY(-1);
-		}
-		
-//	　　　　跳ね返る時に２フレーム描画する
-//		int nextPositionX = ball.getPositionX() + v.getVelocityX();
-//		int nextPositionY = ball.getPositionY() + v.getVelocityY();
-//		
-//		if()
-//		
-//		if(nextPositionX >= Board.BOARD_WIDTH || nextPositionX < 0) {
-//			v.setVelocityX(v.getVelocityX() * -1);
-//		} else if(nextPositionY >= Board.BOARD_HEIGHT || nextPositionY < 0) {
-//			v.setVelocityY(v.getVelocityY() * -1);
-//		} else {
-//			ball.setPositionX(nextPositionX);
-//			ball.setPositionY(nextPositionY);
-//		}
-		
-	}
-	
-//	ボールを追跡するためにパドルの位置を更新するメソッド
-	private static void updatePaddlePosition(DrawnItem ball, DrawnItem paddle) {
-//		パドルの壁との当たり判定
-//		ボールとパドルの真ん中を合わせる
-		int nextPaddlePositionX = ball.getPositionX() - 1;
-		if(nextPaddlePositionX + paddle.getWIDTH() - 1 >= Board.BOARD_WIDTH) {
-			paddle.setPositionX(nextPaddlePositionX - 1);
-		} else if(nextPaddlePositionX < 0) {
-			paddle.setPositionX(0);
-		} else {
-			paddle.setPositionX(nextPaddlePositionX);
-		}
-		
-		
 	}
 	
 }
